bccca92b8751ff86ed203966db59fe58b4644e63
ly4she <ly4she@gmail.com>
2017-08-18 14:15:52 +0300

    1

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	$(document).ready(function(){
+		$('.owl-carousel').owlCarousel({
+			items: 1,
+			nav: true,
+			dots: false,
+			navText: ['<img src="../img/ico/left-ico.png">', '<img src="../img/ico/right-ico.png">']
+		});
+	});
+
+});
